294b55b4acbe3be568bd1af59274409b40a8eaaf
Marcio Bera <marcio.bera@bauermedia.co.uk>
2024-04-13 10:38:50 +0100

    refactor: remove non used app files and non used packages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
     UsersModule,
   ],
   controllers: [],
