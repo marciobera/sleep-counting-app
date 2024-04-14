fc5f29bc430df84ea8d997f5b06366a989666591
Marcio Bera <marcio.bera@bauermedia.co.uk>
2024-04-14 09:53:12 +0100

    test: add minimal unit tests for users.controller

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { Gender, User } from '../model/user.schema';
+import { UserOutDto } from './users.dto';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,13 +11,39 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [UsersService, {
+        provide: getModelToken(User.name),
+        useValue: jest.fn(),
+      },],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('> findAll', () => {
+    it('should return an array of cats', async () => {
+      const result: UserOutDto[] = [
+        <UserOutDto>{ name: 'Foo', gender: Gender.FEMALE, totalHours: 8 },
+        <UserOutDto>{ name: 'Bar', gender: Gender.MALE, totalHours: 10 },
+      ];
+      jest.spyOn(UsersService.prototype, 'findAll').mockReturnValueOnce(Promise.resolve(result));
+      expect(await controller.findAll()).toBe(result);
+    });
+  });
+
+  describe('> getOne', () => {
+    it('should return a user', async () => {
+      const result: User = <User>{ name: 'Foo', gender: Gender.FEMALE, hours: [{ value: 9 }] };
+      jest.spyOn(UsersService.prototype, 'findOne').mockReturnValueOnce(Promise.resolve(result));
+      expect(await controller.getOne('123')).toBe(result);
+    })
+  });
+
+  describe('> createUser', () => {
+    it('should return a user', async () => {
+      const result: User = <User>{ name: 'Foo', gender: Gender.FEMALE, hours: [{ value: 9, date: new Date() }] };
+      jest.spyOn(UsersService.prototype, 'createUser').mockReturnValueOnce(Promise.resolve(result));
+      expect(await controller.createUser(result)).toBe(result);
+    });
   });
 });
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserOutDto } from './users.dto';
-import { User } from 'src/model/user.schema';
+import { User } from '../model/user.schema';
 
 @Controller('/api/v1/users')
 export class UsersController {
